12cbc309efd7d4ac75c91f1ac45d4f7a86f1e794
hugolu <printk@gmail.com>
2017-01-24 17:19:36 +0800

    update ch3.py

--- programming-in-python/ch3.py
+++ programming-in-python/ch3.py
@@ -57,9 +57,53 @@ for i in range(len(l)):
     total += l[i]       # total=10
 
 # list comprehension
+nums = []
+for num in range(1, 50):
+    if (num % 7 == 0 or num % 13 == 0):
+        nums.append(num)
+
+nums    # [7, 13, 14, 21, 26, 28, 35, 39, 42, 49]
+
+nums = [n for n in range(1, 50)
+        if n % 7 == 0 or n % 13 == 0]
 
 ################################################################################
 # Set
+
+s = {7, 'veil', 0, -29, ('x', 11), 'sun', frozenset({8, 4, 7}), 913}
+len(s)  # 8
+
+for item in s:
+    type(item)
+
+# operations
+s = {1, 2, 3}
+s.add(3)        # s={1, 2, 3}
+s.add(4)        # s={1, 2, 3, 4}
+s.clear()       # s=set()
+
+s = {1,2,3}
+s.discard(2)    # s={1,3}
+s.discard(4)    # s={1,3}
+s.remove(3)     # s={1}
+#s.remove(4)     # KeyError Exception
+
+s = {1, 2, 3}
+s.pop()         # 1
+s               # {2, 3}
+
+s = {1, 2, 3}
+t = {2, 3, 4}
+s.isdisjoint(t) # False
+s.issubset(t)   # False
+s.issuperset(t) # False
+
+s = {1, 2, 3}
+t = {2, 3, 4}
+s - t           # {1}           # difference
+s & t           # {2, 3}        # intersection
+s ^ t           # {1, 4}        # symmetric difference
+s | t           # {1, 2, 3, 4}  # union
 
 ################################################################################
 # Map
