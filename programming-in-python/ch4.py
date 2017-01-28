81a9c7284573efdf0c8ee47a0d555a5730ddbe3b
Hugo Lu <printk@gmail.com>
2017-01-28 22:06:08 +0800

    add ch4.py

---
+++ programming-in-python/ch4.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+# Chapter 4 - Control and Function
+
+# conditional branching
+num = 10
+if num % 2 == 0:
+    "{0} is even".format(num)
+else:
+    "{0} is odd".format(num)
